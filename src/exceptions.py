742ee991fcf18d5925b0f589a22cd8cd528a7d8e
Basudev <basuoncloud@gmail.com>
2023-06-06 22:00:20 +0530

    model training and EDA

--- src/exceptions.py
+++ src/exceptions.py
@@ -16,3 +16,10 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+# if __name__ == "__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info('Devide by Zero error')
+#         raise CustomException(e,sys)
